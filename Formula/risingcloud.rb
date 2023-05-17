e788180544aa39356032d00e8293cb2f173ce420
Anthony Moran <anthony@risingcloud.com>
2023-05-16 18:27:37 -0700

    bitbucket pipleline update 1.2.264

--- Formula/risingcloud.rb
+++ Formula/risingcloud.rb
@@ -9,7 +9,7 @@ class Risingcloud < Formula
         sha256 "75da7348327926a3c61f50268aefec493cc4013dfd2ee9eec273ea4782df15d3"
 
         def install
-          bin.install "risingcloud-public.s3-us-west-1.amazonaws.com" => "risingcloud"
+          bin.install "risingcloud_darwin_amd64" => "risingcloud"
         end
       end
       if Hardware::CPU.arm?
@@ -17,7 +17,7 @@ class Risingcloud < Formula
         sha256 "32bde4eca0474fa3265d0f8d12968e0febd43c55026186bed64db26f923efffb"
 
         def install
-          bin.install "risingcloud-public.s3-us-west-1.amazonaws.com" => "risingcloud"
+          bin.install "risingcloud_darwin_arm64" => "risingcloud""
         end
       end
     end
