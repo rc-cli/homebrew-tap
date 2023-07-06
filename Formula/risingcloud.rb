acb52d2ec5abd89f4996f93cb3157d852e123de9
Bitbucket pipeline <bitbucket@risingcloud.com>
2023-07-06 21:53:34 +0000

    bitbucket pipleline update 1.2.289

--- Formula/risingcloud.rb
+++ Formula/risingcloud.rb
@@ -1,28 +1,28 @@
 class Risingcloud < Formula
-    desc ""
-    homepage "https://risingcloud.com"
-    version "1.2.280"
-  
-    on_macos do
-      if Hardware::CPU.intel?
-        url "risingcloud-public.s3-us-west-1.amazonaws.com/risingcloud_darwin_amd64"
-        sha256 "e4a7d0d69a935efa25c92c4238b5492da3892c0594bd8c1bdd05c97ec69f23c3"
+  desc ""
+  homepage "https://risingcloud.com"
+  version "1.2.289"
 
-        def install
-          bin.install "risingcloud-public.s3-us-west-1.amazonaws.com" => "risingcloud"
-        end
-      end
-      if Hardware::CPU.arm?
-        url "risingcloud-public.s3-us-west-1.amazonaws.com/risingcloud_darwin_arm64"
-        sha256 "f07e39fb4853c444be1721617681bb4aa8f0c04aeee699b6091edd682ac2327e"
+  on_macos do
+    if Hardware::CPU.intel?
+      url "risingcloud-public.s3-us-west-1.amazonaws.com/risingcloud-amd64.zip"
+      sha256 "2020a850b3225d97ea8cfa5198a26ee52ea7f9ac13048d740460b280c9cd8685"
 
-        def install
-          bin.install "risingcloud-public.s3-us-west-1.amazonaws.com" => "risingcloud"
-        end
+      def install
+        bin.install "risingcloud"
       end
     end
+    if Hardware::CPU.arm?
+      url "risingcloud-public.s3-us-west-1.amazonaws.com/risingcloud-arm.zip"
+      sha256 "c74a72f6b84ef9b325c7eb606e2faa2d4a16c6780a6b6a59e683cd9e5c68d06c"
 
-    test do
-      system "#{bin}/risingcloud --version"
+      def install
+        bin.install "risingcloud"
+      end
     end
   end
+
+  test do
+    system "#{bin}/risingcloud --version"
+  end
+end
