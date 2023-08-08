209e4f7638e8f917536be3a6ad19765ae2b7a6e7
Bitbucket pipeline <bitbucket@risingcloud.com>
2023-08-08 02:29:18 +0000

    bitbucket pipleline update 1.2.304

--- Formula/risingcloud.rb
+++ Formula/risingcloud.rb
@@ -1,12 +1,12 @@
 class Risingcloud < Formula
   desc ""
   homepage "https://risingcloud.com"
-  version "1.2.300"
+  version "1.2.304"
 
   on_macos do
     if Hardware::CPU.intel?
       url "risingcloud-public.s3-us-west-1.amazonaws.com/risingcloud-amd64.zip"
-      sha256 "dcc3eae6b2b2e183d38ead25579f89b8460d5e8a3d915571f132eba873b908b4"
+      sha256 "18b32b8ac5af1d5fde66d222616ce8db474a0ddfd4f66a40731bdfc90098c912"
 
       def install
         bin.install "risingcloud"
@@ -14,7 +14,7 @@ class Risingcloud < Formula
     end
     if Hardware::CPU.arm?
       url "risingcloud-public.s3-us-west-1.amazonaws.com/risingcloud-arm.zip"
-      sha256 "552e5210d4c819aa855eeb206a7ffd45d81148f60d700f3f081edffebb6a2400"
+      sha256 "ef0ae375af75b0ae72fd7be0247c55d1332143d19d522b9ba141a8670ddc164f"
 
       def install
         bin.install "risingcloud"
